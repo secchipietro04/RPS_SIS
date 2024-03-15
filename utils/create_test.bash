4388b5738cc0ce0bb99edc6b17bfd5dd3cf7dfa3
secchipietro04 <secchi.pietro04@gmail.com>
2024-03-15 13:51:31 +0100

    Working SIS

--- utils/create_test.bash
+++ utils/create_test.bash
@@ -1,15 +1,15 @@
-outputFile="simulate10b.script"
+outputFile="simulate7b.script"
 
 # Clear the existing output file
 > "$outputFile"
 
 # Loop through all binary numbers from 0 to 1023 (2^10 - 1)
-for ((i=0; i<=1023; i++)); do
+for ((i=0; i<=127; i++)); do
     binaryNumber=""
     
     # Convert the decimal number to binary
     decimalNumber="$i"
-    for ((j=9; j>=0; j--)); do
+    for ((j=6; j>=0; j--)); do
         bit=$(( (decimalNumber >> j) & 1 ))
         binaryNumber="$binaryNumber $bit"
     done
